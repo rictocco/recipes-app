721321796872e71b0fef96c82b24e80af8408ed4
rtocco <ricardo.tocco@gmail.com>
2018-09-05 19:32:46 +0100

    added all files

---
+++ src/main/java/com/auyamatech/RecipesAppApplication.java
@@ -0,0 +1,12 @@
+package com.auyamatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesAppApplication.class, args);
+	}
+}
