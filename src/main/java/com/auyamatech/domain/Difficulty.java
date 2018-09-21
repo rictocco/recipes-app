487405a638a5d65860892a1b872614dd9667a589
rtocco <ricardo.tocco@gmail.com>
2018-09-21 12:34:51 +0100

    Created the Java jpa entities for the recipe app.

---
+++ src/main/java/com/auyamatech/domain/Difficulty.java
@@ -0,0 +1,8 @@
+package com.auyamatech.domain;
+
+public enum Difficulty {
+    EASY,
+    MEDIUM,
+    KIND_OF_HARD,
+    HARD
+}
---
+++ src/main/java/com/auyamatech/domain/UnitOfMesure.java
@@ -0,0 +1,28 @@
+package com.auyamatech.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMesure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
